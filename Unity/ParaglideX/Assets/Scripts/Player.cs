49b9587d04b80807b050cd2b08afc5a5f1ed1377
Pontus Stjernström <Pontus Stjernstrm>
2016-02-11 16:31:23 +0100

    Trying to add the first basic script.

---
+++ Unity/ParaglideX/Assets/Scripts/Player.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+	public const float PLAYER_WEIGHT = 80f;
+	public const float GRAVITY = -9.807f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		CharacterController controller = GetComponent<CharacterController> ();
+
+		//If the player is on ground
+		if (controller.isGrounded) {
+			controller.Move (new Vector3 (1, 0, 0) * Time.deltaTime);
+		} else {//Apply gravity!
+			controller.Move(new Vector3(0,GRAVITY, 0)*Time.deltaTime);
+		}
+	}
+}
