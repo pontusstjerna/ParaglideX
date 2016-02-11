2bb8cea60cc3644bc99d2aee8958fcf18cb7ae29
Pontus Stjernström <Pontus Stjernstrm>
2016-02-11 16:56:00 +0100

    Added simple movement, gravity and mouse looking/controller.

--- Unity/ParaglideX/Assets/Scripts/Player.cs
+++ Unity/ParaglideX/Assets/Scripts/Player.cs
@@ -4,22 +4,40 @@ using System.Collections;
 public class Player : MonoBehaviour {
 
 	public const float PLAYER_WEIGHT = 80f;
-	public const float GRAVITY = -9.807f;
+	public const float GRAVITY = 9.807f;
+
+	private Vector3 moveDirection = Vector3.zero;
+	private CharacterController controller;
+	private float speed = 6;
+
+	private const float MOUSE_SENSITIVITY = 0.3f;
 
 	// Use this for initialization
 	void Start () {
-		
+		controller = GetComponent<CharacterController> ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		CharacterController controller = GetComponent<CharacterController> ();
-
 		//If the player is on ground
 		if (controller.isGrounded) {
-			controller.Move (new Vector3 (1, 0, 0) * Time.deltaTime);
+
+			//Add the user inputs. X is sideways, Z is forward/backward.
+			moveDirection = new Vector3 (Input.GetAxis ("sideways"), 0, Input.GetAxis("forward"));
+
+			//Translate the direction to world space
+			moveDirection = transform.TransformDirection (moveDirection);
+
+			//Apply the speed!
+			moveDirection *= speed;
+
 		} else {//Apply gravity!
-			controller.Move(new Vector3(0,GRAVITY, 0)*Time.deltaTime);
+			moveDirection.y -= GRAVITY;
 		}
+
+		//Apply movement
+		controller.Move(moveDirection * Time.deltaTime);
+
+		transform.Rotate (0, Input.GetAxis ("mouseX")*MOUSE_SENSITIVITY, 0);
 	}
 }
