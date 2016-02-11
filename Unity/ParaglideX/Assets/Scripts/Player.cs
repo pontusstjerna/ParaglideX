002aeff2b6daf655dd1e6c163d0909819ab86367
Pontus Stjernström <Pontus Stjernstrm>
2016-02-11 22:39:50 +0100

    Temporarly fixed the view bug. Now, the camera rotates around the global y-axis instead of the gliders. But I can't change that until I've created a glider...

--- Unity/ParaglideX/Assets/Scripts/Player.cs
+++ Unity/ParaglideX/Assets/Scripts/Player.cs
@@ -10,11 +10,14 @@ public class Player : MonoBehaviour {
 	private CharacterController controller;
 	private float speed = 6;
 
-	private const float MOUSE_SENSITIVITY = 0.3f;
+	private const float MOUSE_SENSITIVITY = 3f;
 
 	// Use this for initialization
 	void Start () {
 		controller = GetComponent<CharacterController> ();
+
+		//Turn of the cursor while in fps
+		Cursor.visible = false;
 	}
 	
 	// Update is called once per frame
@@ -22,13 +25,18 @@ public class Player : MonoBehaviour {
 		//If the player is on ground
 		if (controller.isGrounded) {
 
+			//Sprint
+			if (Input.GetKey (KeyCode.LeftShift)) {
+				speed = 12;
+			}
+
 			//Add the user inputs. X is sideways, Z is forward/backward.
 			moveDirection = new Vector3 (Input.GetAxis ("sideways"), 0, Input.GetAxis("forward"));
 
 			//Translate the direction to world space
 			moveDirection = transform.TransformDirection (moveDirection);
 
-			//Apply the speed!
+			//Apply the speed! 
 			moveDirection *= speed;
 
 		} else {//Apply gravity!
@@ -38,6 +46,11 @@ public class Player : MonoBehaviour {
 		//Apply movement
 		controller.Move(moveDirection * Time.deltaTime);
 
-		transform.Rotate (0, Input.GetAxis ("mouseX")*MOUSE_SENSITIVITY, 0);
+		//Rotate vertically around the local x-axis.
+		transform.Rotate(Input.GetAxis("mouseY")*Time.deltaTime*-MOUSE_SENSITIVITY, 0,0, Space.Self);
+
+		//This should actually not be Space.World but Space.Paraglider, 
+		//since if in a turn, you should look horizontally relative to the glider.
+		transform.Rotate (0, Input.GetAxis ("mouseX")*Time.deltaTime*MOUSE_SENSITIVITY, 0, Space.World);
 	}
 }
