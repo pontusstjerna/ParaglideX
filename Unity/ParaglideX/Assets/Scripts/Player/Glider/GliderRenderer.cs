cb774442a0f34bb70e6064a304582b6473f89893
Pontus Stjernström <Pontus Stjernstrm>
2016-05-15 12:15:08 +0200

    Done the animations correct now I think. Still need to script them right.

--- Unity/ParaglideX/Assets/Scripts/Player/Glider/GliderRenderer.cs
+++ Unity/ParaglideX/Assets/Scripts/Player/Glider/GliderRenderer.cs
@@ -3,34 +3,24 @@ using System.Collections;
 
 public class GliderRenderer : MonoBehaviour {
 
-	private Animation animations;
+	private Animator animator;
 
 	// Use this for initialization
 	void Start () {
-		animations = GetComponent<Animation> ();
-		animations ["GliderArmature|LeftBrakeArmatureAction"].AddMixingTransform (GetComponent<Transform> ());
+		animator = GetComponent<Animator> ();
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetAxis("brakeL") > 0) {
-			LeftBrakeFrame ();
-		} else if (Input.GetAxis("brakeR") > 0) {
-			RightBrakeFrame ();
-		} else {
-			animations.Stop ();
-		}
+
 	}
 
 	private void LeftBrakeFrame(){
-		//1 = 30 frames because time is in seconds.
-		animations["GliderArmature|LeftBrakeArmatureAction"].time = Input.GetAxis("brakeL");
-		animations.Play ();
+
 	}
 
 	private void RightBrakeFrame(){
-		//1 = 30 frames because time is in seconds.
-		animations["GliderArmature|LeftBrakeArmatureAction"].time = 1.1f + Input.GetAxis("brakeR");
-		animations.Play ();
+
 	}
 }
